d21d17af49fa9eb21c8c126f30efba04b3ab0913
Robinson <robinpaulista@yahoo.com.br>
2020-11-01 12:34:27 -0300

    Tarefa avaliativa em for e while

--- robinson.dias24.ex.cpp
+++ robinson.dias24.ex.cpp
@@ -11,19 +11,25 @@ Digite a opção desejada.
 Na opção 1: receber o salário de um funcionário, calcular e ar e mostrar o valor do imposto
 usando as regras a seguir.
 
-Tabela 
+    Salários                          % do Imposto
+menor que R$500,00,                        5
+De R$500,00, a R$850,00                    10
+Acima de R$850,00                          15
 
 
 Na opção 2: receber o salário de um funcionário, calcular e mostrar o valor do novo
 salário usando as regras a seguir.
 
-Tabela
+    Salários                                        Aumento
+Maiores que R$1500,00                               R$25,00
+De R$750,00(inclusive) a R$1500,00(inclusive)       R$50,00
+De R$450,00(inclusive) a R$750,00                   R$75,00
+Menores que R$450,00                                R$100,00
 
 
 Na opção 3: receber o salário de um funcionário e mostrar sua classificação usando
 esta tabela:
 
-Tabela
-
-
-*/
+    Salários                                Classificação
+Até R$700,00 R$                             Mal remunerado
+Maiores que R$700,00 R$                     Bem remunerado             */
