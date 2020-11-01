d1493acf15ceea0ab86f182969d8fb87e17aa47d
Robinson <robinpaulista@yahoo.com.br>
2020-11-01 10:38:18 -0300

    Tarefa avaliativa em for e while

--- robinson.dias.ex9.cpp
+++ robinson.dias.ex9.cpp
@@ -1,8 +1,33 @@
 /* 9. Faça um programa que receba duas notas de seis alunos. Calcule e mostre:
+
+Média aritmética                         Mensagem
+Até 3                                    Reprovado
+Entre 3 e 7                                Exame
+De 7 para cima                           Aprovado
+
 ■ a média aritmética das duas notas de cada aluno; e
 ■ a mensagem que está na tabela a seguir:
 ■ o total de alunos aprovados;
 ■ o total de alunos de exame;
 ■ o total de alunos reprovados;
 ■ a média da classe. */
+
+#include <stdio.h>
+int main(){
+    float n1, n2, media_aritmetica, mensagem, aprovados, reprovados, exame, media_classe;
+    printf("\nDigite uma nota: \n");
+    scanf("%f",&n1);
+    printf("\nDigite a segunda nota: \n");
+    scanf("%f",&n2);
+
+    media_aritmetica = (n1+n2)/2;
+
+    printf("\nA media aritmetica do aluno e: %.2f \n", media_aritmetica);
+
+
+
+    getchar();
+    return 0;
+
+}
 
--- robinson.dias15.ex.cpp
+++ robinson.dias15.ex.cpp
@@ -1,6 +1,22 @@
 /* 15. Faça um programa que receba o preço unitário, a refrigeração (S para os 
-produtos que necessitem de refrigeração e N para os que não necessitem) e a categoria 
-(A — alimentação; L — limpeza; e V — vestuário) de doze produtos, e que calcule e mostre:
+produtos que necessitem de refrigeração e N para os que não necessitem) e a 
+categoria (A — alimentação; L — limpeza; e V — vestuário) de doze produtos, 
+e que calcule e mostre:
+
+Preço Unitário            Refrigeração       Categoria             Custo de Estocagem
+                          ------------            A                   R$2,00
+Até 20                    ------------            L                   R$3,00
+                          ------------            v                   R$4,00
+Entre 20 e 50(inclusive)    S               ----------------          R$6,00
+Entre 20 e 50(inclusive)    N               ----------------          R$0,00
+
+Menor que 50                S                      A                  R$5,00
+Menor que 50                S                      L                  R$2,00
+Menor que 50                S                      V                  R$4,00
+Menor que 50                N                    A ou V               R$0,00            
+Menor que 50                N                      L                  R$1,00
+
+ 
 ■ O custo de estocagem, calculado de acordo com a tabela a seguir.
 ■ O imposto calculado de acordo com as regras a seguir:
 Se o produto não preencher nenhum dos requisitos a seguir, seu imposto será de 2%
