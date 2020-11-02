40c34c7d41d879043f250ec5dcb8f31c9816ea0d
Robinson <robinpaulista@yahoo.com.br>
2020-11-02 08:45:07 -0300

    Tarefa avaliativa em for e while

--- robinson.dias2.cpp
+++ robinson.dias2.cpp
@@ -4,9 +4,16 @@ digitados). */
 
 #include <stdio.h>
 int main(){
-    int n1, n2
+    int n1, n2, impar = 0;
+    printf("Digite dois numeros: \n");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
 
-
+    for (int n1 = 1; n1 <= n2; n1++){
+        impar = n1%2;
+        if(impar)
+            printf("\n%d\n",n1);
+    }
 
     getchar();
     return 0;
